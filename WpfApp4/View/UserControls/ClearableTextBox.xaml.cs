3b10e65f68061910d9fce35347d9441ede0ed29d
Stuti Dutta <stutidutta@aarkglobalinc.com>
2024-06-05 10:53:15 +0530

    Add project files.

---
+++ WpfApp4/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp4.View.UserControls
+{
+
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set { 
+                placeholder = value; 
+                tbPlaceholder.Text = placeholder;
+            }
+        }
+
+
+        private void btnClass_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
